a5c0fbb7d2c4c6f972c4bf8fc8c33f7ae2a18a33
las3qj <las3qj@virginia.edu>
2022-09-12 19:10:44 -0400

    classwork

--- Assets/Scripts/MoveObject.cs
+++ Assets/Scripts/MoveObject.cs
@@ -2,14 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveObject : MonoBehaviour
+public class MoveObject : MonoBehaviour // MoveObject inherits from MonoBehaviour; anything which will become a component will also
 {
     [SerializeField] float moveSpeed;
     [SerializeField] Vector3 moveDirection;
+    [SerializeField] GameObject stationarySphere;
 
     float totalMoveDistance;
     Vector3 startingLocation;   
 
+    // These next two are inherited from MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
@@ -27,7 +29,7 @@ public class MoveObject : MonoBehaviour
             FlipMoveDirection();
         }
 
-        //gameObject.transform.Translate(moveDirection * moveSpeed);
+        gameObject.transform.Translate(moveDirection * moveSpeed);
     }
 
     void FlipMoveDirection()
